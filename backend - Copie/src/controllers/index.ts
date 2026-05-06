ef93f44e6bc44c5d3a88107b872b9cb4626c8c90
ABOUBACAR SIDIKI KOUROUMA <dvlpdsd@gmail.com>
2026-05-06 14:16:57 +0000

    Remove backend - Copie from tracking

--- backend - Copie/src/controllers/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as authController } from './auth.controller';
-export { default as employeesController } from './employees.controller';
-export { default as badgesController } from './badges.controller';
--- backend - Copie/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as authService } from './auth.service';
-export { default as employeeService } from './employee.service';
-export { default as badgeService } from './badge.service';
